181c38530eb48a76c7c85f1b7c7fc64c602c3c53
Ioannis Obreque <ioannis.isob@gmail.com>
2023-11-02 03:01:20 -0300

    fecha entrega (prueba)

--- feria_virtual_web/Transportista.aspx.cs
+++ feria_virtual_web/Transportista.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,50 @@ namespace feria_virtual_web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            BindGrid();
+        }
 
+        private void BindGrid()
+        {
+            string connectionString = "Data Source=localhost:1521/xe;User Id=maipogrande;Password=123;";
+
+            using (OracleConnection con = new OracleConnection(connectionString))
+            {
+                OracleCommand cmd = new OracleCommand("SELECT * FROM cabecera_subasta", con);
+                con.Open();
+                OracleDataAdapter sda = new OracleDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                GridViewSubasta.DataSource = dt;
+                GridViewSubasta.DataBind();
+            }
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ActualizarFechaEntrega")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow row = GridViewSubasta.Rows[index];
+                string idSubasta = row.Cells[0].Text;
+                ActualizarFechaDespachoRealizado(idSubasta);
+                BindGrid();
+            }
+        }
+
+        private void ActualizarFechaDespachoRealizado(string idCabeceraSubasta)
+        {
+            string connectionString = "Data Source=localhost:1521/xe;User Id=maipogrande;Password=123;";
+            using (OracleConnection con = new OracleConnection(connectionString))
+            {
+                string query = "UPDATE cabecera_subasta SET FECHA_DESPACHO_REALIZADO = SYSDATE WHERE ID_CABECERA_SUBASTA = :idCabeceraSubasta";
+                using (OracleCommand cmd = new OracleCommand(query, con))
+                {
+                    cmd.Parameters.Add(":idCabeceraSubasta", OracleDbType.Varchar2).Value = idCabeceraSubasta;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
