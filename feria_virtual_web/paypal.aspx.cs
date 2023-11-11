34bd89704c1feaa1fcc215cc8ce8ecdb424f1b59
brian aguila <brianagui98@gmail.com>
2023-11-11 19:17:54 -0300

    paypal integracion

---
+++ feria_virtual_web/paypal.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace feria_virtual_web
+{
+    public partial class paypal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Pagar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("https://www.sandbox.paypal.com/checkoutnow?token=2FK27307GB1809422");
+        
+        }
+    }
+}
