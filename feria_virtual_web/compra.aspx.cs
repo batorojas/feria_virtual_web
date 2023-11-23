0617b8070a0ee4a886f0271284904719b7a52ba8
brian aguila <brianagui98@gmail.com>
2023-11-22 21:05:55 -0300

    continuo paypal

--- feria_virtual_web/compra.aspx.cs
+++ feria_virtual_web/compra.aspx.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using Oracle.ManagedDataAccess.Client;
-using System.Drawing;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
-
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Oracle.ManagedDataAccess.Client;
 
 namespace feria_virtual_web
 {
@@ -19,8 +16,6 @@ namespace feria_virtual_web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-
         }
 
         protected void btnConsultar_Click(object sender, EventArgs e)
@@ -35,7 +30,6 @@ namespace feria_virtual_web
                 {
                     con.Open();
 
-                    // Consulta SQL modificada según tu requerimiento
                     string selectQuery = "SELECT DP.ID_DETALLE_PV, DP.ID_PRODUCTO, DP.CANTIDAD, DP.PRECIO_UNITARIO " +
                                          "FROM DETALLE_PV DP " +
                                          "INNER JOIN CABECERA_PV CP ON DP.ID_CABECERA_PV = CP.ID_CABECERA_PV " +
@@ -45,12 +39,10 @@ namespace feria_virtual_web
                     {
                         cmd.Parameters.Add(new OracleParameter("Rut", rut));
 
-                        // Crear un adaptador y un conjunto de datos para almacenar los resultados de la consulta
                         OracleDataAdapter adapter = new OracleDataAdapter(cmd);
                         DataTable dt = new DataTable();
                         adapter.Fill(dt);
 
-                        // Enlazar los datos al GridView
                         gvDetallesPV.DataSource = dt;
                         gvDetallesPV.DataBind();
                     }
@@ -60,12 +52,9 @@ namespace feria_virtual_web
             }
             catch (Exception ex)
             {
-                // Manejar errores aquí (mostrar un mensaje de error, registrar el error, etc.)
                 Response.Write("Error rut no encontrado: " + ex.Message);
             }
         }
-
-
 
         protected async void gvDetallesPV_RowCommand(object sender, GridViewCommandEventArgs e)
         {
@@ -83,23 +72,29 @@ namespace feria_virtual_web
                         GridViewRow row = gvDetallesPV.Rows[rowIndex];
 
                         // Obtener los valores de las celdas directamente del GridView
-                        string idDetallePV = row.Cells[0].Text; // Ajusta el índice según la posición de la columna en tu GridView
-                        string idProducto = row.Cells[1].Text;  // Ajusta el índice según la posición de la columna en tu GridView
-                        string cantidad = row.Cells[2].Text;     // Ajusta el índice según la posición de la columna en tu GridView
-                        string precioUnitario = row.Cells[3].Text; // Ajusta el índice según la posición de la columna en tu GridView
+                        string idDetallePV = row.Cells[0].Text;
+                        string idProducto = row.Cells[1].Text;
+                        string cantidad = row.Cells[2].Text;
+                        string precioUnitario = row.Cells[3].Text;
 
                         // Realizar la multiplicación de cantidad por precio unitario
                         decimal total = Convert.ToDecimal(cantidad) * Convert.ToDecimal(precioUnitario);
 
+                        // Formatear el total con dos decimales y punto como separador decimal
+                        string montoFormateado = total.ToString("F2", CultureInfo.InvariantCulture);
+
                         // Obtener el token de acceso de PayPal
                         string accessToken = await ObtenerTokenAccesoPayPal().ConfigureAwait(false);
 
-                        // Construir la URL de PayPal con el token de acceso y el monto total
-                        string urlPayPal = $"https://www.sandbox.paypal.com/checkoutnow?token={accessToken}&amount={total}";
-                        Response.Write($"URL de redirección: {urlPayPal}");
-                        Response.Redirect(urlPayPal);
+                        // Crear la orden de PayPal
+                        string orderId = await CrearOrdenPayPal(accessToken, total).ConfigureAwait(false);
 
-                        // Redirigir a la página de PayPal con el token de acceso
+                        // Construir la URL de PayPal con el token de acceso y el ID de la orden
+                        string urlPayPal = $"https://www.sandbox.paypal.com/checkoutnow?token={accessToken}&orderId={orderId}";
+
+                        Response.Write($"URL de redirección: {urlPayPal}");
+
+                        // Redirigir a la página de PayPal con el token de acceso y el ID de la orden
                         Response.Redirect(urlPayPal);
                     }
                 }
@@ -111,48 +106,83 @@ namespace feria_virtual_web
             }
         }
 
-        // Método para obtener el token de acceso de PayPal
         private async Task<string> ObtenerTokenAccesoPayPal()
         {
             using (HttpClient client = new HttpClient())
             {
-                // Configurar las credenciales de la aplicación PayPal (reemplaza con las tuyas)
                 string clientId = "AeSjmd1cOUZF164S1qM0n7Ul_ZAnX9K_oHyZd6fW_NLaWZvKX4vdWOIR5TVDvgEk4UJWvG62sj8JCFgt";
                 string clientSecret = "EC913X7vIu-2icepQF_Lg9nn64GI9IRVchzW2s0gT8XnosB8mQOaPnukHfmmuWDNoB3VL4Mr1xDDiUTu";
 
-                // Codificar las credenciales en base64
                 string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{clientId}:{clientSecret}"));
 
-                // Configurar los datos de la solicitud
                 var requestData = new List<KeyValuePair<string, string>>
                 {
                     new KeyValuePair<string, string>("grant_type", "client_credentials")
                 };
 
-                // Configurar las cabeceras de la solicitud
                 client.DefaultRequestHeaders.Add("Authorization", $"Basic {credentials}");
 
-                // Realizar la solicitud para obtener el token de acceso
-                HttpResponseMessage response = await client.PostAsync("https://api.sandbox.paypal.com/v1/oauth2/token", new FormUrlEncodedContent(requestData));
+                HttpResponseMessage response = await client.PostAsync("https://api-m.sandbox.paypal.com/v1/oauth2/token", new FormUrlEncodedContent(requestData));
 
-                // Leer y deserializar la respuesta
                 if (response.IsSuccessStatusCode)
                 {
                     var responseData = await response.Content.ReadAsStringAsync();
-                    // Devolver el token de acceso
-                    return responseData;
+                    return JsonConvert.DeserializeObject<PayPalTokenResponse>(responseData)?.access_token;
                 }
                 else
                 {
-                    // Manejar el error de la solicitud
                     string errorMessage = await response.Content.ReadAsStringAsync();
                     throw new Exception($"Error al obtener el token de acceso de PayPal: {errorMessage}");
                 }
             }
         }
 
+        private async Task<string> CrearOrdenPayPal(string accessToken, decimal total)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
 
+                var requestData = new
+                {
+                    intent = "CAPTURE",
+                    purchase_units = new[]
+                    {
+                        new
+                        {
+                            amount = new
+                            {
+                                
+                                currency_code = "USD",
+                                value = total.ToString("F2", CultureInfo.InvariantCulture) // Formatear el total con dos decimales
+                            }
+                        }
+                    }
+                };
 
+                string jsonRequest = JsonConvert.SerializeObject(requestData);
 
+                HttpResponseMessage response = await client.PostAsync("https://api-m.sandbox.paypal.com/v2/checkout/orders", new StringContent(jsonRequest, Encoding.UTF8, "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    dynamic orderData = JsonConvert.DeserializeObject(responseData);
+                    return orderData.id;
+                }
+                else
+                {
+                    string errorMessage = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Error al crear la orden de PayPal: {errorMessage}");
+                }
+            }
+        }
+    }
+
+    public class PayPalTokenResponse
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
     }
 }
